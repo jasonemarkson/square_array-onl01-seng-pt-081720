779b612e39602ae6d9ff2c72fc165e2f657765b1
Jason Markson <jasonmarkson76@gmail.com>
2020-08-23 16:25:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ numbers = [1, 2, 3]
 def square_array(numbers)
   sq_numbers = []
   numbers.each do |element| 
-    sq_numbers << [element*element]
+    sq_numbers << element*element
     end
    return sq_numbers
 end
